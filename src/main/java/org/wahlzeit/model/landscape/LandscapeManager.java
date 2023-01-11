5482bbdcfa7185454647f6a5f757bf07e35f152b
lara <lara.sulzbach@fau.de>
2023-01-11 14:54:38 +0100

    [HA9] - added type objects

---
+++ src/main/java/org/wahlzeit/model/landscape/LandscapeManager.java
@@ -0,0 +1,72 @@
+package org.wahlzeit.model.landscape;
+
+import java.util.HashMap;
+
+public class LandscapeManager {
+
+    private LandscapeType root;
+    private HashMap<Integer, Landscape> landscapes = new HashMap<>();
+    private HashMap<String, LandscapeType> landscapeTypes = new HashMap<>();
+
+    protected static LandscapeManager instance = new LandscapeManager();
+
+
+    public LandscapeManager(){
+        initializeLandscapeTypes();
+    }
+
+    public static LandscapeManager getInstance() {
+        return instance;
+    }
+
+    /**
+     * creates a new landscape instance
+     * @param landscapeType type of the landscape
+     * @return new landscape instance
+     */
+    public Landscape createLandscape(String landscapeType){
+        LandscapeType type = landscapeTypes.get(landscapeType);
+        if(type == null){
+            createLandscapeType(landscapeType, null);
+        }
+        Landscape result = type.createInstance();
+        landscapes.put(result.getId(), result);
+        return result;
+    }
+
+    /**
+     * creates a new landscapeType
+     * @param typeString value of the landscapeType
+     * @param superTypeString supertype of it, default is root
+     * @return new landscapeType, a subtype of the superTypeString
+     */
+    public LandscapeType createLandscapeType(String typeString, String superTypeString){
+        LandscapeType result = new LandscapeType(typeString);
+        LandscapeType superType = landscapeTypes.get(superTypeString);
+        if(superType != null){
+            superType.addSubType(result);
+        }
+        else{
+            root.addSubType(result);
+        }
+        landscapeTypes.put(typeString, result);
+        return result;
+    }
+
+    /**
+     * creates the initial hierachy of landscapeTypes
+     */
+    private void initializeLandscapeTypes(){
+        for(LandscapeTypesValues values : LandscapeTypesValues.values()){
+            landscapeTypes.put(values.toString(), new LandscapeType(values.toString()));
+        }
+        this.root = landscapeTypes.get(LandscapeTypesValues.WORLD.toString());
+        landscapeTypes.get(LandscapeTypesValues.WORLD.toString()).addSubType(landscapeTypes.get(LandscapeTypesValues.NATURE.toString()));
+        landscapeTypes.get(LandscapeTypesValues.WORLD.toString()).addSubType(landscapeTypes.get(LandscapeTypesValues.URBAN.toString()));
+        landscapeTypes.get(LandscapeTypesValues.NATURE.toString()).addSubType(landscapeTypes.get(LandscapeTypesValues.MOUNTAINS.toString()));
+        landscapeTypes.get(LandscapeTypesValues.NATURE.toString()).addSubType(landscapeTypes.get(LandscapeTypesValues.SEA.toString()));
+        landscapeTypes.get(LandscapeTypesValues.NATURE.toString()).addSubType(landscapeTypes.get(LandscapeTypesValues.DESERT.toString()));
+        landscapeTypes.get(LandscapeTypesValues.URBAN.toString()).addSubType(landscapeTypes.get(LandscapeTypesValues.CITY.toString()));
+        landscapeTypes.get(LandscapeTypesValues.URBAN.toString()).addSubType(landscapeTypes.get(LandscapeTypesValues.TOWN.toString()));
+    }
+}
---
+++ src/main/java/org/wahlzeit/model/landscape/LandscapeTypesValues.java
@@ -0,0 +1,15 @@
+package org.wahlzeit.model.landscape;
+
+/**
+ * Enum with values for the initial hierachy of landscapetypes
+ */
+public enum LandscapeTypesValues {
+    WORLD,
+    URBAN,
+    NATURE,
+    MOUNTAINS,
+    SEA,
+    DESERT,
+    CITY,
+    TOWN,
+}
